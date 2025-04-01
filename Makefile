393c585daeff877dd19fb39e626d687e88a4d890
Yue-Peng Tsuei <anthony@Mac.lan>
2025-04-01 11:12:47 -0400

    no coverage boilerplate

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
-RUSTFLAGS_COVERAGE := -C instrument-coverage
+RUSTFLAGS+=-g
+RUSTFLAGS+=-Cpasses=sancov-module
+RUSTFLAGS+=-Cllvm-args=-sanitizer-coverage-trace-pc-guard
+RUSTFLAGS+=-Clink-dead-code
+RUSTFLAGS+=-Cforce-frame-pointers=yes
+RUSTFLAGS+=-Ctarget-feature=-crt-static
+RUSTFLAGS+=-Cinstrument-coverage
 
 .phony: all clean
 
 all:
-	RUSTFLAGS="$(RUSTFLAGS_COVERAGE)" cargo build --release -Z build-std=std,panic_abort
+	RUSTFLAGS="$(RUSTFLAGS)" cargo build --release --target aarch64-apple-darwin
 
 clean:
 	cargo clean
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,96 @@
+#![allow(static_mut_refs)]
+
+use std::path::PathBuf;
+
+use libafl_bolts::tuples::tuple_list;
 use serde_json;
 use json;
 
+use libafl::{
+    corpus::{CachedOnDiskCorpus, Corpus, OnDiskCorpus}, events::SimpleEventManager, executors::{inprocess::InProcessExecutor, ExitKind}, feedback_or, feedback_or_fast, feedbacks::{CrashFeedback, MaxMapFeedback, TimeFeedback, TimeoutFeedback}, fuzzer::{Fuzzer, StdFuzzer}, inputs::{BytesInput, HasTargetBytes}, monitors::SimpleMonitor, mutators::{havoc_mutations, StdScheduledMutator}, observers::{HitcountsMapObserver, StdMapObserver, TimeObserver}, schedulers::QueueScheduler, stages::mutational::StdMutationalStage, state::{HasCorpus, StdState}
+};
+
+use libafl_targets::coverage::EDGES_MAP;
+
 fn main() {
+    let mon = SimpleMonitor::new(|s| println!("{s}"));
+    let mut mgr = SimpleEventManager::new(mon);
+    
+    let edges_observer = 
+        HitcountsMapObserver::new(unsafe { StdMapObserver::new("edges", &mut EDGES_MAP) });
+
+    let time_observer = TimeObserver::new("time");
+
+    let mut feedback = feedback_or!(
+        MaxMapFeedback::new(&edges_observer),
+        TimeFeedback::new(&time_observer)
+    );
+
+    let mut objective = 
+        feedback_or_fast!(CrashFeedback::new(), TimeoutFeedback::new());
+    
+    let mut state = StdState::new(
+        libafl_bolts::rands::StdRand::with_seed(0),
+        CachedOnDiskCorpus::<BytesInput>::new("./corpus", 1024).unwrap(),
+        OnDiskCorpus::new(PathBuf::from("./crashes")).unwrap(),
+        &mut feedback,
+        &mut objective,
+    ).unwrap();
+
+    let scheduler = QueueScheduler::new();
+
+    let mut fuzzer = StdFuzzer::new(scheduler, feedback, objective);
+
+    let mut harness = |input: &BytesInput| -> ExitKind {
+
+        let input_bytes = input.target_bytes();
+        if let Ok(input_str) = str::from_utf8(&input_bytes) {
+            let serde_json_parsed: Result<serde_json::Value, serde_json::Error> = serde_json::from_str(input_str);
+    
+            let json_parsed = json::parse(input_str);
+    
+            if serde_json_parsed.is_ok() != json_parsed.is_ok() {
+                eprintln!("mismatch on return type:\n{:?}\n{:?}", serde_json_parsed, json_parsed);
+                return ExitKind::Crash;
+            }
+
+            // TODO: add state diffing
+        }
+        
+        unsafe {
+            if EDGES_MAP.iter().find(|&v| *v > 0).is_some() {
+                println!("Found a new edge!");
+            }
+        }
+        ExitKind::Ok
+    };
+
+    let mut executor = InProcessExecutor::new(
+        &mut harness,
+        tuple_list!(edges_observer, time_observer),
+        &mut fuzzer,
+        &mut state,
+        &mut mgr
+    ).unwrap();
+
+    let mutator = StdScheduledMutator::new(havoc_mutations());
+    let mut stages = tuple_list!(StdMutationalStage::new(mutator));
+
+    if state.corpus().count() < 1 {
+        state
+            .load_initial_inputs_forced(
+                &mut fuzzer,
+                &mut executor,
+                &mut mgr,
+                &["./corpus".into()],
+            )
+            .unwrap();
+    }
+
+    println!("Starting fuzzing loop...");
+    fuzzer.fuzz_loop(&mut stages, &mut executor, &mut state, &mut mgr).unwrap();
+
+    println!("Fuzzing finished.");
 }
 
 #[cfg(test)]
